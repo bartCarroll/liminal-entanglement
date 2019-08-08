e87d64ac528de3b2610d6bfd9d4d85554e6baee2
Bart Carroll <bart.a.carroll@gmail.com>
2019-08-07 20:57:24 -0500

    Keyboard interaction and threading working

--- terminal/terminal_main.py
+++ terminal/terminal_main.py
@@ -1,14 +1,27 @@
 import sqlite3
 import time
+import keyboard
 from effects import Effects
 from flipdot import client, display
-from pynput import keyboard
+from threading import Thread, Lock
+
+
+
+# CONFIGS
+SIMULATED = True
+SIMULATED_IP = "192.168.86.63"
+SIMULATED_PORT = 9999
+FLIPDOT_USB = '/dev/ttyUSB0'
+KEYBOARD_USB = '/dev/ttyUSB1'
 
 d = display.Display(28, 14,
                     panels={
                         2: ((0, 0), (28, 7)),
                         1: ((0, 7), (28, 7)),
                     })
+
+display_lock = Lock()
+key_pressed = False
 
 
 # Builds dictionary from cursor result
@@ -29,9 +42,12 @@ def create_connection(db_file):
     return None
 
 
-def on_press(key):
-    print('key pressed')
-    return False  # Disable listener thread.
+def on_press(event):
+    global key_pressed
+    print('key pressed' + str(event))
+    display_lock.acquire()
+    key_pressed = True
+    display_lock.release()
 
 
 def format_categories(categories):
@@ -43,8 +59,9 @@ def format_categories(categories):
 
 def do_user_interaction(cur, categories, cat_str, d):
     count = 0
+
     while not count == 3:
-        print(cat_str)
+        Effects.scroll_text(d, cat_str)
         selection = input("> ").lower()
         selected_cat = next((item for item in categories if item["display"].lower() == selection), None)
         print(selected_cat)
@@ -54,8 +71,13 @@ def do_user_interaction(cur, categories, cat_str, d):
                         str(selected_cat['id']))
             question = cur.fetchone()
             Effects.scroll_text(d, question['text'], font=Effects.SmallFont)
-            Effects.wipe_down(d)
+            Effects.display_text(d, 'Enter your\n Selection', font=Effects.SmallFont)
+            events = keyboard.record(until="enter")
+            print("events: " + str(events))
+            text = next(keyboard.get_typed_strings(events))
 
+            print(str(text))
+            Effects.scroll_text(d, text, font=Effects.SmallFont)
             break
 
 
@@ -63,30 +85,47 @@ def transition(disp):
     Effects.rand(disp)
 
 
-if __name__ == "__main__":
-    print("terminal main init...")
+def waiting_thread(disp):
+    print("Started wiating thread...")
+    while 1:
+        display_lock.acquire()
+        Effects.rand(d)
+        display_lock.release()
+        time.sleep(1)
 
+
+def interaction_thread(disp):
+    global key_pressed
     con = create_connection('data/le_data.db')
-    d.connect(client.UDPClient("localhost", 9999))
-
     con.row_factory = dict_factory
     with con:
         cur = con.cursor()
         cur.execute("SELECT * FROM CATEGORY")
         categories = cur.fetchall()
         cat_str = format_categories(categories)
-        listener = keyboard.Listener(on_press=on_press)
-        listener.start()
-        while 1:
-            Effects.display_text(d, "Hey")
-            time.sleep(2)
-            transition(d)
-            Effects.blink_text(d, "Hi")
-            transition(d)
-            Effects.rand(d)
-            Effects.scroll_text(d, "Please talk to me")
-            Effects.gobble(d)
-            if not listener.is_alive():
+        keyboard.hook(on_press)
+        while True:
+            display_lock.acquire()
+            if key_pressed:
+                keyboard.unhook_all()
                 do_user_interaction(cur, categories, cat_str, d)
-                listener = keyboard.Listener(on_press=on_press)
-                listener.start()
+                key_pressed = False
+                keyboard.hook(on_press)
+            display_lock.release()
+            time.sleep(1)
+
+
+if __name__ == "__main__":
+    print("terminal main init...")
+
+    if SIMULATED:
+        d.connect(client.UDPClient(SIMULATED_IP, SIMULATED_PORT))
+    else:
+        d.connect(client.SerialClient(FLIPDOT_USB))
+
+    wait_thread = Thread(target=waiting_thread, args=(d,), daemon=True)
+    wait_thread.start()
+    interact_thread = Thread(target=interaction_thread, args=(d,), daemon=True)
+    interact_thread.start()
+    wait_thread.join()
+    interact_thread.join()
