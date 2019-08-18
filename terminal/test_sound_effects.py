8904e778d112a174cc4a96cedf3faf0f0b7b6252
Bart Carroll <bart.a.carroll@gmail.com>
2019-08-17 20:57:47 -0500

    added ogg files.

--- terminal/test_sound_effects.py
+++ terminal/test_sound_effects.py
@@ -1,15 +1,17 @@
 import time
 from itertools import cycle
 
-import SoundEffects
+from effects import SoundEffects
 
 
 def mainloop():
     try_again_itr = cycle(SoundEffects.try_again)
     boundary_select_itr = cycle(SoundEffects.boundary_selection)
     while True:
-        SoundEffects.play_sound(next(try_again_itr))
-        SoundEffects.play_sound(next(boundary_select_itr))
+        next(try_again_itr).play()
+        time.sleep(5)
+        next(boundary_select_itr).play()
+        time.sleep(5)
 
 
 def main():
